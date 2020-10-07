2f3c993fd31cc6e05e8077f0160b3c7d7cf0677e
Juanito Ortega <jortega@grundfos.com>
2020-10-07 13:36:14 +0800

    clean up for new development

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,4 @@
 import "package:flutter/material.dart";
-import 'app_screens/first_screen.dart';
 
-void main()=> runApp(new SampleApp());
+void main()=> runApp(null);
 
-class SampleApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "Sample Application",
-        home: Scaffold(
-            appBar: AppBar(title: Text("Sample App - App Bar")),
-            body:FirstScreen())
-    );
-  }
-
-}
