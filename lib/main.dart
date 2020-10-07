d6bf61c32fa5398f776ac9c046e6571737c53da2
Juanito Ortega <jortega@grundfos.com>
2020-10-07 11:16:48 +0800

    organizing Dart code

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,27 @@
 import "package:flutter/material.dart";
 
 void main(){
-  runApp(
-      MaterialApp(
+  runApp(new SampleApp());
+}
+
+class SampleApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
         title: "Sample Application",
         home: Scaffold(
-          appBar: AppBar(title: Text("Sample App - App Bar")),
-          body: Material(
-              color: Colors.redAccent,
-              child:
-              Center(
+            appBar: AppBar(title: Text("Sample App - App Bar")),
+            body: Material(
+                color: Colors.redAccent,
                 child:
-                Text("Hello World!",
-                    textDirection:TextDirection.ltr,
-                    style: TextStyle(color: Colors.white, fontSize: 40.0)),
-              )
-          ))
-      )
-  );
+                Center(
+                  child:
+                  Text("Hello World!",
+                      textDirection:TextDirection.ltr,
+                      style: TextStyle(color: Colors.white, fontSize: 40.0)),
+                )
+            ))
+    );
+  }
+
 }
