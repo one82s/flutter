a70e526f0590c2e72e12fef940cd6fe5c6a1a452
Juanito Ortega <jortega@grundfos.com>
2020-10-07 17:27:30 +0800

    Row widget implementation

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -1,29 +1,31 @@
 import "package:flutter/material.dart";
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
+  static const fontSize = 35.0;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:Container(
-          alignment: Alignment.center,
-          color: Colors.redAccent,
-          margin:EdgeInsets.all(50.0),
-          child:getText()
-      )
-    );
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.redAccent,
+            margin: EdgeInsets.all(50.0),
+            child: Row(
+              children: [
+                getText("Cebu Pacific"),
+                getText("From Manila to Siargao")
+              ],
+            )));
   }
 
-  Text getText(){
-    return Text("Flight", textDirection: TextDirection.ltr,
-      style:TextStyle(
-          decoration: TextDecoration.none,
-          fontSize: 75.0,
-          fontFamily: 'Raleway',
-          fontWeight: FontWeight.w300,
-          fontStyle: FontStyle.italic,
-          color: Colors.white
-      )
-    );
+  Text getText(String text) {
+    return Text(text,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: fontSize,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w300,
+            fontStyle: FontStyle.italic,
+            color: Colors.white));
   }
-
 }
