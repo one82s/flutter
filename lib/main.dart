cf56f16d81ae7dddfcde2e2c4508d822f9828360
Juanito Ortega <jortega@grundfos.com>
2020-10-07 09:16:05 +0800

    designing UI with Dart widgets e.g. MaterialApp, Scaffold, AppBar and etc.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,20 @@ import "package:flutter/material.dart";
 
 void main(){
   runApp(
-      Center(
-        child:
-        Text("Hello World!",
-            textDirection:TextDirection.ltr),
+      MaterialApp(
+        title: "Sample Application",
+        home: Scaffold(
+          appBar: AppBar(title: Text("Sample App - App Bar")),
+          body: Material(
+              color: Colors.redAccent,
+              child:
+              Center(
+                child:
+                Text("Hello World!",
+                    textDirection:TextDirection.ltr,
+                    style: TextStyle(color: Colors.white, fontSize: 40.0)),
+              )
+          ))
       )
-    );
+  );
 }
