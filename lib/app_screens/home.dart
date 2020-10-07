97bff54e52c110235100416de6b1848dfd4a6b5e
Juanito Ortega <jortega@grundfos.com>
2020-10-07 14:04:22 +0800

    wrapping Container inside Center to have its own width and height

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -3,15 +3,20 @@ import "package:flutter/material.dart";
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Container(
-        alignment: Alignment.center,
-        color: Colors.redAccent,
-        child:getText()
+    return Center(
+      child:Container(
+          alignment: Alignment.center,
+          color: Colors.redAccent,
+          width:200.0,
+          height: 100.0,
+          child:getText()
+      )
     );
   }
 
   Text getText(){
-    return Text("Flight", textDirection: TextDirection.ltr);
+    return Text("Flight", textDirection: TextDirection.ltr
+    );
   }
 
 }
