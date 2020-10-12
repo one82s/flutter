3c93485dcfc4350f3772802efabf796f06aa3fad
Juanito Ortega <jortega@grundfos.com>
2020-10-12 14:45:12 +0800

    bank image added

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,30 @@ class SIForm extends StatefulWidget{
 
 class _SIFormState extends State<SIForm>{
   var _currencies = ['Peso', 'Dollar', 'Euro', 'Yen'];
+  final _minimumPadding = 5.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Simple Intereset Calculator')
       ),
-      body:Column(
-        children: [],
+      body:Container(
+        margin:EdgeInsets.all(_minimumPadding*2),
+        child:Column(
+          children: [
+            getImageAsset(),
+          ],
+        )
+
       )
     );
+  }
+
+  Widget getImageAsset(){
+    AssetImage assetImage = AssetImage('images/bank_icon.png');
+    Image image = Image(image:assetImage, width: 125.0, height: 125.0);
+    return Container(child: image, margin: EdgeInsets.all(_minimumPadding*10));
+
   }
 
 }
