543a1720a10a17316e8f04646cf9efb9af354cce
Juanito Ortega <jortega@grundfos.com>
2020-10-13 11:11:41 +0800

    implemented calculation and reset of values

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,8 @@ class SIForm extends StatefulWidget{
 }
 
 class _SIFormState extends State<SIForm>{
-  var _currencies = ['Peso', 'Dollar', 'Euro', 'Yen'];
-  var currencySelected = 'Peso';
+  var _currencies = ['Pesos', 'Dollars', 'Euros', 'Yen'];
+  var currencySelected = '';
   final _minimumPadding = 5.0;
   final _principalText = 'Principal';
   final _principalHintText = 'Enter Principal e.g. 1200';
@@ -34,6 +34,16 @@ class _SIFormState extends State<SIForm>{
   final _roiHintText = 'In percent';
   final _termText = 'Term';
   final _termHintText = 'Terms in years';
+  TextEditingController _principalController = TextEditingController();
+  TextEditingController _roiController = TextEditingController();
+  TextEditingController _termController = TextEditingController();
+  String calculationMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    this.currencySelected = _currencies[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,16 +58,16 @@ class _SIFormState extends State<SIForm>{
           children: [
             getImageAsset(),
             getColumnChildWithPadding(
-               getTextField(_principalText, _principalHintText)
+               getTextField(_principalText, _principalHintText, _principalController)
             ),
             getColumnChildWithPadding(
-               getTextField(_roiText, _roiHintText)
+               getTextField(_roiText, _roiHintText, _roiController)
             ),
             getColumnChildWithPadding(
               Row(
                 children: [
                   Expanded(
-                    child: getTextField(_termText, _termHintText)
+                    child: getTextField(_termText, _termHintText, _termController)
                   ),
                   Container(
                     width: _minimumPadding * 5
@@ -72,7 +82,7 @@ class _SIFormState extends State<SIForm>{
                       }).toList(),
                       value: currencySelected,
                       onChanged: (String newValueSelected){
-
+                        _onDropdownItemSelected(newValueSelected);
                       })
                   )
                 ],
@@ -81,17 +91,17 @@ class _SIFormState extends State<SIForm>{
             getColumnChildWithPadding(
               Row(
                 children: [
-                  getExpandedButtons('Calculate'),
+                  getExpandedButtons('Calculate', true),
                   Container(
                       width: _minimumPadding
                   ),
-                  getExpandedButtons('Reset')
+                  getExpandedButtons('Reset', false)
                  ]
               )
             ),
             Padding(
               padding: EdgeInsets.all(_minimumPadding*2),
-              child: Text('TODO: Output text here'),
+              child: Text(this.calculationMessage),
             )
           ]
         )
@@ -107,13 +117,13 @@ class _SIFormState extends State<SIForm>{
 
   }
 
-  TextField getTextField(String labelText, String hintText){
+  TextField getTextField(String labelText, String hintText, TextEditingController textController){
     return   TextField(
       keyboardType: TextInputType.number,
+      controller: textController,
       decoration: InputDecoration(
         labelText: labelText,
         hintText: hintText,
-
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(_minimumPadding),
         ),
@@ -130,14 +140,42 @@ class _SIFormState extends State<SIForm>{
     );
   }
 
-  Expanded getExpandedButtons(String buttonText){
+  Expanded getExpandedButtons(String buttonText, bool isCalculate){
     return Expanded(
           child: RaisedButton(
               child: Text(buttonText),
               onPressed: (){
-                debugPrint('$buttonText Button was pressed');
+//                debugPrint('$buttonText Button was pressed');
+                setState((){
+                  if(isCalculate){
+                    this.calculationMessage = _calculateTotalAmount();
+                  }
+                  else{
+                    _reset();
+                  }
+                });
               }
           )
     );
+  }
+
+  void _onDropdownItemSelected(String newValueSelected){
+    setState(() => this.currencySelected = newValueSelected);
+  }
+
+  String _calculateTotalAmount (){
+      double principal = double.parse(_principalController.text);
+      double roi = double.parse(_roiController.text);
+      double term = double.parse(_termController.text);
+      double totalAmount = principal + (principal*roi*term)/100;
+    return "Your total investment is $totalAmount $currencySelected";
+  }
+
+  void _reset(){
+    this._principalController.text = '';
+    this._roiController.text = '';
+    this._termController.text = '';
+    this.currencySelected = _currencies[0];
+    this.calculationMessage = '';
   }
 }
