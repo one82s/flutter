f3388f21c57bcf50fcd82c1bde34b44a3ec237ca
Juanito Ortega <jortega@grundfos.com>
2020-10-16 14:48:51 +0800

    UI implementation of add and edit screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firstapp/screens/note_list.dart';
+import 'package:firstapp/screens/note_detail.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: NoteList(),
+      home: NoteDetail(),
     );
   }
 }
--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -1,1 +1,125 @@
 import 'package:flutter/material.dart';
+
+class NoteDetail extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return NodeDetailState();
+  }
+
+}
+
+class NodeDetailState extends State<NoteDetail>{
+  var priorities = ['High', 'Medium', 'Low'];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.headline6;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Note'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top:15.0, left: 10.0, right: 10.0),
+        child: ListView(
+          children: [
+            ListTile(
+              title: DropdownButton(
+                items: priorities.map((String dropDownItems){
+                  return DropdownMenuItem<String>(
+                    value: dropDownItems,
+                    child: Text(dropDownItems),
+                  );
+                }).toList(),
+                style: textStyle,
+                value: 'Low',
+                onChanged:(selectedValue){
+                  setState((){
+                    debugPrint('User selected $selectedValue');
+                  });
+                },
+
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: TextField(
+                controller: titleController,
+                style: textStyle,
+                onChanged: (textValue){
+                  debugPrint('Title entered is $textValue');
+                },
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: TextField(
+                controller: descriptionController,
+                style: textStyle,
+                onChanged: (textValue){
+                  debugPrint('Description entered is $textValue');
+                },
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                            'Save', textScaleFactor: 1.5
+                        ),
+                        onPressed: (){
+                          setState((){
+                            debugPrint('Save button was pressed');
+                          });
+                        },
+                      )
+                  ),
+                  Container(
+                      width: 5.0
+                  ),
+                  Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                            'Reset', textScaleFactor: 1.5
+                        ),
+                        onPressed: (){
+                          setState((){
+                            debugPrint('Reset button was pressed');
+                          });
+                        },
+                      )
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget getNoteDetailView(){
+    return null;
+  }
+}
