5fba45b8e167502c5670fd9674af262747946ec8
Juanito Ortega <jortega@grundfos.com>
2020-10-17 12:21:56 +0800

    added model class as initial SQLite connection implementation

---
+++ lib/models/note.dart
@@ -0,0 +1,65 @@
+class Note{
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Note(this._title, this._date, this._priority, [this._description]);
+
+  Note.withId(this._id, this._title, this._date,
+      this._priority, [this._description]);
+
+  int get priority => _priority;
+
+  set priority(int value) {
+    if(value >=1 && value<=3){
+      _priority = value;
+    }
+  }
+
+  String get date => _date;
+
+  set date(String value) {
+    _date = value;
+  }
+
+  String get description => _description;
+
+  set description(String value) {
+    if(value.length<=255){
+      _description = value;
+    }
+  }
+
+  String get title => _title;
+
+  set title(String value) {
+    if(value.length<=255){
+      title = value;
+    }
+  }
+
+  int get id => _id;
+
+  Map<String, dynamic> toMap(){
+    var map = Map<String,dynamic>();
+    if(id!=null){
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['description'] = _description;
+    map['date'] = _date;
+    map['priority'] = _priority;
+
+    return map;
+  }
+
+  Note.fromMapObject(Map<String, dynamic> map){
+    _id = map['title'];
+    _title = map['title'];
+    _description = map['description'];
+    _date = map['date'];
+    _priority = map['priority'];
+  }
+}
