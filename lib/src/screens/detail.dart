905e21291820986b6605d7a797af0cfa0fd51155
Juanito Ortega <jortega@grundfos.com>
2020-11-03 13:47:46 +0800

    detail section carousel - initial implementation

--- lib/src/screens/detail.dart
+++ lib/src/screens/detail.dart
@@ -1,6 +1,7 @@
 import 'package:firstapp/src/models/product.dart';
 import 'package:firstapp/src/utils/commons.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class Detail extends StatefulWidget {
   final Product product;
@@ -19,7 +20,23 @@ class _DetailState extends State<Detail> {
       body: SafeArea(
         child: Column(
           children: [
-            Text(widget.product.name, style: TextStyle(color: white))
+//            Image.asset(widget.product.image)
+            Container(
+              height: 300.0,
+              child: Carousel(
+                images: [
+                  AssetImage(widget.product.image),
+                  AssetImage(widget.product.image),
+                  AssetImage(widget.product.image)
+                ],
+                dotBgColor: red,
+                dotColor: grey,
+                dotIncreasedColor: white,
+                dotIncreaseSize: 1.5,
+              ),
+            )
+//            IconButton(icon: Icon(Icons.arrow_back_ios),onPressed: (){Navigator.pop(context);}, color: white,),
+//            Text(widget.product.name, style: TextStyle(color: white))
           ],
         ),
       ),
