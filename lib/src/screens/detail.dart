fbc3114526dadf06b104520d4fabe21edc5a1b54
Juanito Ortega <jortega@grundfos.com>
2020-11-04 14:09:15 +0800

    detail section - cart in carousel

--- lib/src/screens/detail.dart
+++ lib/src/screens/detail.dart
@@ -1,5 +1,6 @@
 import 'package:firstapp/src/models/product.dart';
 import 'package:firstapp/src/utils/commons.dart';
+import 'package:firstapp/src/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 
@@ -23,16 +24,60 @@ class _DetailState extends State<Detail> {
 //            Image.asset(widget.product.image)
             Container(
               height: 300.0,
-              child: Carousel(
-                images: [
-                  AssetImage(widget.product.image),
-                  AssetImage(widget.product.image),
-                  AssetImage(widget.product.image)
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Carousel(
+                      images: [
+                        AssetImage(widget.product.image),
+                        AssetImage(widget.product.image),
+                        AssetImage(widget.product.image)
+                      ],
+                      dotBgColor: red,
+                      dotColor: grey,
+                      dotIncreasedColor: white,
+                      dotIncreaseSize: 1.2,
+                      autoplay: false,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(icon: Icon(Icons.arrow_back_ios),onPressed: (){Navigator.pop(context);}, color: white),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Stack(
+                            children: [
+                              Icon(Icons.shopping_cart, color: white, size: 30.0),
+                              Positioned(
+                                right: 3.0,
+                                top: 1.0,
+                                child: Container(
+                                    width: 18.0,
+                                    decoration: BoxDecoration(
+                                      color: red,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      boxShadow:[
+                                        BoxShadow(
+                                          color: grey,
+                                          offset: Offset(2,1),
+                                          blurRadius: 3
+                                        )
+                                      ]
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 4.0, top: 2.0),
+                                      child: CustomText(text: "2", color: white, size: 10, weight: FontWeight.bold,),
+                                    )
+                                ),
+                              )
+                            ]
+                        )
+                      )
+                    ],
+                  ),
                 ],
-                dotBgColor: red,
-                dotColor: grey,
-                dotIncreasedColor: white,
-                dotIncreaseSize: 1.5,
               ),
             )
 //            IconButton(icon: Icon(Icons.arrow_back_ios),onPressed: (){Navigator.pop(context);}, color: white,),
